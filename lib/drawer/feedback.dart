793e316f01aba87af0edc4958feb050ecf623678
Chris <alejaga.jct@gmail.com>
2024-07-23 14:22:47 +0800

    Update (Settings & Feedback Fixed)

--- lib/drawer/feedback.dart
+++ lib/drawer/feedback.dart
@@ -1,22 +1,167 @@
 import 'package:eyesync/components/color.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:eyesync/components/color.dart';
 
-class FeedbackScreen extends StatelessWidget {
-  const FeedbackScreen({super.key});
+class FeedbackScreen extends StatefulWidget {
+  const FeedbackScreen({Key? key}) : super(key: key);
 
-@override
-    Widget build(BuildContext context){
-  return Scaffold(
-    backgroundColor: Colors.white,
-    appBar: AppBar(
-      iconTheme: const IconThemeData(color: Colors.white),
-      title: Text("Feedback", style: GoogleFonts.poppins(fontSize: 20, color: secondaryColor),),
-      backgroundColor: primaryColor,
-    ),
-    body: const Center(
-      child: Text("Dito nakalagay deets"),),
-  );
-  
-    }
+  @override
+  _FeedbackScreenState createState() => _FeedbackScreenState();
+}
+
+class _FeedbackScreenState extends State<FeedbackScreen> {
+  double _rating = 0;
+  final TextEditingController _messageController = TextEditingController();
+
+  void _submitFeedback() {
+    // Handle the feedback submission logic here
+    String message = _messageController.text;
+    // For demonstration, we'll just show a snackbar
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Feedback submitted: Rating - $_rating, Message - $message')),
+    );
+    // Clear the message field
+    _messageController.clear();
+    // Reset the rating
+    setState(() {
+      _rating = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          'Feedback',
+          style: GoogleFonts.poppins(fontSize: 20, color: Colors.white),
+        ),
+        backgroundColor: primaryColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'We value your feedback!',
+              style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold, color: primaryColor),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Please rate your experience:',
+              style: GoogleFonts.poppins(fontSize: 18, color: Colors.black),
+            ),
+            SizedBox(height: 10),
+            _buildRatingBar(),
+            SizedBox(height: 20),
+            Text(
+              'Your Message:',
+              style: GoogleFonts.poppins(fontSize: 18, color: Colors.black),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _messageController,
+              maxLines: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your message here...',
+              ),
+              style: GoogleFonts.poppins(),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: _submitFeedback,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: primaryColor,
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Text(
+                  'Send Feedback',
+                  style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            _buildAdditionalFeatures(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRatingBar() {
+    return Row(
+      children: List.generate(5, (index) {
+        return IconButton(
+          icon: Icon(
+            index < _rating ? Icons.star : Icons.star_border,
+            color: Colors.amber,
+            size: 36,
+          ),
+          onPressed: () {
+            setState(() {
+              _rating = index + 1.0;
+            });
+          },
+        );
+      }),
+    );
+  }
+
+  Widget _buildAdditionalFeatures() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Additional Features:',
+          style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold, color: primaryColor),
+        ),
+        SizedBox(height: 10),
+        ListTile(
+          leading: Icon(Icons.question_answer, color: primaryColor),
+          title: Text(
+            'FAQs',
+            style: GoogleFonts.poppins(),
+          ),
+          onTap: () {
+            // Navigate to FAQ screen
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.contact_support, color: primaryColor),
+          title: Text(
+            'Contact Support',
+            style: GoogleFonts.poppins(),
+          ),
+          onTap: () {
+            // Navigate to Contact Support screen
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.bug_report, color: primaryColor),
+          title: Text(
+            'Report a Bug',
+            style: GoogleFonts.poppins(),
+          ),
+          onTap: () {
+            // Navigate to Bug Report screen
+          },
+        ),
+      ],
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: FeedbackScreen(),
+  ));
 }
--- lib/drawer/settings.dart
+++ lib/drawer/settings.dart
@@ -254,7 +254,14 @@ class _SettingsScreenState extends State<SettingsScreen> {
                         padding: const EdgeInsets.only(right: 1.0),
                         child: const Icon(Icons.arrow_forward_ios_rounded, color: thirdyColor),
                       ),
-                      onTap: () {},
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PasswordScreen(),
+                          ),
+                        );
+                      },
                     ),
                     ListTile(
                       leading: Icon(Icons.info, color: thirdyColor),
@@ -353,6 +360,139 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
         ],
       ),
+    );
+  }
+}
+
+
+class PasswordScreen extends StatefulWidget {
+  const PasswordScreen({Key? key}) : super(key: key);
+
+  @override
+  _PasswordScreenState createState() => _PasswordScreenState();
+}
+
+class _PasswordScreenState extends State<PasswordScreen> {
+  bool _isTwoFactorEnabled = false;
+  final TextEditingController _currentPasswordController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  void _changePassword() {
+    // Implement password change logic here
+    String currentPassword = _currentPasswordController.text;
+    String newPassword = _newPasswordController.text;
+    String confirmPassword = _confirmPasswordController.text;
+
+    if (newPassword != confirmPassword) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('New passwords do not match')),
+      );
+      return;
+    }
+
+    // Assume the password change is successful
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Password changed successfully')),
+    );
+
+    // Clear text fields
+    _currentPasswordController.clear();
+    _newPasswordController.clear();
+    _confirmPasswordController.clear();
+  }
+
+  void _toggleTwoFactorAuthentication(bool value) {
+    setState(() {
+      _isTwoFactorEnabled = value;
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Two-Factor Authentication ${value ? "enabled" : "disabled"}')),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          'Settings',
+          style: GoogleFonts.poppins(fontSize: 20, color: Colors.white),
+        ),
+        backgroundColor: primaryColor,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20.0),
+        children: [
+          Text(
+            'Security',
+            style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold, color: primaryColor),
+          ),
+          SizedBox(height: 20),
+          ListTile(
+            leading: Icon(Icons.lock, color: primaryColor),
+            title: Text('Change Password', style: GoogleFonts.poppins(fontSize: 18)),
+            onTap: () {
+              _showChangePasswordDialog();
+            },
+          ),
+          SwitchListTile(
+            title: Text('Enable Two-Factor Authentication', style: GoogleFonts.poppins(fontSize: 18)),
+            value: _isTwoFactorEnabled,
+            onChanged: _toggleTwoFactorAuthentication,
+            secondary: Icon(Icons.security, color: primaryColor),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showChangePasswordDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Change Password', style: GoogleFonts.poppins(fontWeight: FontWeight.bold)),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  controller: _currentPasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Current Password'),
+                ),
+                TextField(
+                  controller: _newPasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'New Password'),
+                ),
+                TextField(
+                  controller: _confirmPasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Confirm New Password'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel', style: GoogleFonts.poppins(color: primaryColor)),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: Text('Change', style: GoogleFonts.poppins()),
+              onPressed: () {
+                _changePassword();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
